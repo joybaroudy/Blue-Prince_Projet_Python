5a490fb53de4eb69c778f7b1262e2deeda649070
Albert <albertboghossian2002@gmail.com>
2025-11-16 22:14:00 +0100

    modification dependance circulaire

--- Source_Code/Objets.py
+++ Source_Code/Objets.py
@@ -1,35 +1,35 @@
-from Inventory import Inventaire
+# from Inventory import Inventaire
 
-class Objet:
-    """Classe de base pour tout objet du jeu."""
-    def __init__(self, nom=None):
-        self.nom = nom
+# class Objet:
+#     """Classe de base pour tout objet du jeu."""
+#     def __init__(self, nom=None):
+#         self.nom = nom
 
-class ObjetConsommable(Objet):
-    """Objets consommables (Pas, Pièces, Gemmes, Clés, Dés)."""
-    def __init__(self, nom, quantite=0):
-        super().__init__(nom)
-        self.quantite = quantite
+# class ObjetConsommable(Objet):
+#     """Objets consommables (Pas, Pièces, Gemmes, Clés, Dés)."""
+#     def __init__(self, nom, quantite=0):
+#         super().__init__(nom)
+#         self.quantite = quantite
 
-    def changer_solde(self, changement):
-        """Augmente ou diminue la quantité."""
-        self.quantite = self.quantite + changement
+#     def changer_solde(self, changement):
+#         """Augmente ou diminue la quantité."""
+#         self.quantite = self.quantite + changement
         
-class ObjetPermanent(Objet):
-    """Objet permanent (Shovel, Hammer, etc.)."""
-    def __init__(self, nom, obtenu=False):
-        super().__init__(nom)
-        self.obtenu = obtenu
+# class ObjetPermanent(Objet):
+#     """Objet permanent (Shovel, Hammer, etc.)."""
+#     def __init__(self, nom, obtenu=False):
+#         super().__init__(nom)
+#         self.obtenu = obtenu
 
-    def debloquer(self):
-        self.obtenu = True
+#     def debloquer(self):
+#         self.obtenu = True
 
-class Nourriture(Objet):
-    """Nourriture - donne des pas."""
-    def __init__(self, nom, gain):
-        super().__init__(nom)
-        self.gain = gain
+# class Nourriture(Objet):
+#     """Nourriture - donne des pas."""
+#     def __init__(self, nom, gain):
+#         super().__init__(nom)
+#         self.gain = gain
 
-    def consommer(self, inventaire : Inventaire ):
-        inventaire.objets_consommables["Pas"].changer_solde(self.gain)
+#     def consommer(self, inventaire : Inventaire ):
+#         inventaire.objets_consommables["Pas"].changer_solde(self.gain)
 
