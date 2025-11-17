298d849b49d2acb7b3666abcfdc4f73c995e69a5
Albert <albertboghossian2002@gmail.com>
2025-11-17 06:42:41 +0100

    corr

--- Source_Code/EffetsSalles.py
+++ Source_Code/EffetsSalles.py
@@ -8,7 +8,7 @@ class EffetsSalles :
          self.couleur = salles.salle_couleur_dict[str(ID)]
          self.name = salles.salles_names_dict[str(ID)]
     
-    def apply_entry_effets(self, inventaire : Inventaire) :
+    def apply_entry_effects(self, inventaire : Inventaire) :
          
         if self.couleur == "Purple" :
             inventaire.objets_consommables["Pas"].changer_solde(int(+2))
